a0bb2f701f06a8d8b038a72d0ee84576ca63d726
ChaoYuan99 <91030022+ChaoYuan99@users.noreply.github.com>
2021-12-05 16:27:31 -0500

    Add files via upload

---
+++ Scripts/TOP10Platform_sales.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(ggthemes)
+library(ggplot2)
+library(reshape2)
+library(data.table)
+library(hrbrthemes)
+library(gganimate)
+library(ggThemeAssist)
+library(gridExtra)
+library(gapminder)
+
+Platform_sales <- vgsales %>% group_by(Platform) %>% summarise(sum_global_sales = sum(Global_Sales), .groups = 'drop') %>% 
+  arrange(desc(sum_global_sales))
+
+ggplot(data = head(Platform_sales, 10), aes(x = Platform, y = sum_global_sales, fill = Platform)) + 
+  geom_bar(stat = 'identity') +
+  geom_label(aes(label = sum_global_sales), size = 3) +
+  theme_classic() +
+  coord_flip() +
+  labs(title = "Top 10 Global Sales By Platform", y = "Global Sales",colour = "Blue")
