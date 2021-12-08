9feac732115c0324e5a78b8da7c51f96a31f94e4
ChaoYuan99 <91030022+ChaoYuan99@users.noreply.github.com>
2021-12-07 23:52:01 -0500

    Update TOP10Publisher_sales.R

--- Scripts/TOP10Publisher_sales.R
+++ Scripts/TOP10Publisher_sales.R
@@ -9,6 +9,8 @@ library(ggThemeAssist)
 library(gridExtra)
 library(gapminder)
 
+vgsales <- read.csv("Data/vgsales.csv")
+
 Publisher_sales <- vgsales %>% group_by(Publisher) %>% summarise(sum_global_sales = sum(Global_Sales), .groups = 'drop') %>% 
   arrange(desc(sum_global_sales))
 
@@ -18,4 +20,4 @@ p4 = ggplot(data = head(Publisher_sales, 10), aes(x = Publisher, y = sum_global_
   theme_classic() +
   coord_flip() +
   labs(title = "Top 10 Global Sales By Publisher", y = "Global Sales",colour = "Blue")
-ggsave("TOP10Publisher.png",p4)
+ggsave("Figures/TOP10Publisher_sales.png",p4)
